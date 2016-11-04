e223d4fe7c7725086a064db9c338619e377d09c7
Kit Yan <tszkit0903@gmail.com>
2016-11-04 11:15:53 +0800

    uncomment check device has bluetooth

---
+++ app/src/main/java/com/example/kit/personalalarm/ConnectedThread.java
@@ -0,0 +1,8 @@
+package com.example.kit.personalalarm;
+
+/**
+ * Created by Kit on 3/11/2016.
+ */
+
+class ConnectedThread {
+}
--- app/src/main/java/com/example/kit/personalalarm/MainActivity.java
+++ app/src/main/java/com/example/kit/personalalarm/MainActivity.java
@@ -241,16 +241,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null)
         {
-//            new AlertDialog.Builder(this)
-//                    .setTitle("Not compatible")
-//                    .setMessage("Your phone does not support Bluetooth")
-//                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
-//                        public void onClick(DialogInterface dialog, int which) {
-//                            System.exit(0);
-//                        }
-//                    })
-//                    .setIcon(android.R.drawable.ic_dialog_alert)
-//                    .show();
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
         else {
             if (!bluetoothAdapter.isEnabled()) {
